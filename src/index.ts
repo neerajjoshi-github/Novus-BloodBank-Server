3e5351f7c3d461b47b4cd6daea7d5ac4d4a1c5ab
neerajjoshi-github <joshineeraj7037@gmail.com>
2023-10-25 18:22:08 +0530

    Updated package.json to deploy

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import app from "./app";
 
 const connectionURL = process.env.MONGODB_CONNECTION_URL || "";
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 main().catch((err) =>
   console.log("Error occurred during connecting to database", err)
@@ -13,6 +13,6 @@ async function main() {
   await mongoose.connect(connectionURL);
   console.log("Database connected successfully!!");
   app.listen(port, () => {
-    console.log(`Server listening at port ${port}`);
+    console.log(`Server listening at port ${port}!!`);
   });
 }
